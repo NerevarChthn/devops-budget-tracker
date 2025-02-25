9118fcb2b57424b7401191548c4fae3d911bcc22
Nobody0117 <johannes.keiner@outlook.de>
2025-02-25 17:39:09 +0100

    get und insert

---
+++ budget-tracker-frontend/backend/insert.php
@@ -0,0 +1,54 @@
+<?php
+	/**phpinfo();*/
+	if(!isset($_GET["date"])){
+		die(json_encode(['state' => 'error', 'data' => 'date is missing']));
+	}else if (!isset($_GET["money"])){
+		die(json_encode(['state' => 'error', 'data' => 'money is missing']));
+	}else if (!isset($_GET["type"])){
+		die(json_encode(['state' => 'error', 'data' => 'type is missing']));
+	}else if (!isset($_GET["description"])){
+		die(json_encode(['state' => 'error', 'data' => 'description is missing']));
+	}else {
+		$date = date_create($_GET["date"]);
+		$money = floatval($_GET["money"]);
+		$type = $_GET["type"];
+		$description = $_GET["description"];
+	}
+	
+	if (!$date){
+		die(json_encode(['state' => 'error', 'data' => 'date has the wrong format']));
+	}else if ($money === 0 ){
+		die(json_encode(['state' => 'error', 'data' => 'money is 0']));
+	}else if(!($type === "income" or $type === "expense")){
+		die(json_encode(['state' => 'error', 'data' => 'type must be income or expense']));
+	}else if(strlen($description) > 250){
+		die(json_encode(['state' => 'error', 'data' => 'description is longer then 250']));
+	}else{
+		if($type === "income")
+			$intake = 1;
+		if($type === "expense")
+			$intake = 0;
+	}
+	
+	$conf = include('db-conn.php');
+	$server = $conf['server'];
+	$user = $conf['user'];
+	$password = $conf['password'];
+	
+	try {
+		$conn = new PDO("mysql:host=$server;dbname=budget", $user, $password);
+		// set the PDO error mode to exception
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$date_format = date_format($date,"Y-m-d");
+		
+		$conn->query("INSERT INTO planning (date, money, intake, description) VALUES('$date_format', $money, $intake, '$description')");
+		
+		echo json_encode(['state' => 'success', 'data' => 'none']);
+	} catch(PDOException $e) {
+		echo json_encode(['state' => 'error', 'data' => $e->getMessage()]);
+	}
+	
+	
+	
+?>
